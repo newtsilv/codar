8a5cfd7e58b8f4a36eb8d9bc16eecfd7061da54f
neewt19 <newthonaraujo19@gmail.com>
2025-12-10 14:37:55 -0300

    adiciona os arquivos principais da pagina inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSection } from "./pages/hero-section/hero-section";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/hero-section/hero-section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from "../../shared/components/header/header";
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [Header],
+  templateUrl: './hero-section.html',
+  styleUrl: './hero-section.css',
+})
+export class HeroSection {
+
+}
