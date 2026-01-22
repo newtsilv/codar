6c888a39938fcad8cca14ac0e9303f7c19968750
neewt19 <newthonaraujo19@gmail.com>
2026-01-22 13:26:15 -0300

    feat:adciona header, hero section e o footer

--- src/app/pages/hero-section/hero-section.ts
+++ src/app/pages/hero-section/hero-section.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../../shared/components/header/header";
+import { Card } from './components/card/card';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [Header],
+  imports: [Header, Footer, Card],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.css',
 })
